b19d7baa18fc08dc7ad72fcbfe05fa0b87000766
Rishub Tamirisa <rishubt2@illinois.edu>
2022-02-26 01:30:22 -0600

    simplified training loop

--- model.py
+++ model.py
@@ -27,40 +27,37 @@ def train():
     cv = RepeatedKFold(n_splits=10, n_repeats=3, random_state=1)
     # enumerate folds
     max = 0
-    for train_ix, test_ix in cv.split(x):
-        # prepare data
-        # x_train, y_train, x_test, y_test = data.Data.create_data()
-        x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.50,random_state=2)
+    # for train_ix, test_ix in cv.split(x):
+    # prepare data
+    # x_train, y_train, x_test, y_test = data.Data.create_data()
+    x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.50,random_state=2)
 
-        # x_train, x_test = x[train_ix], x[test_ix]
-        # y_train, y_test = y[train_ix], y[test_ix]
-        #build model
-        # model = net.model(numInputs=8, numOutputs=2)
-        # fit model
-        # print("==================TRAINING=====================")
-        # model.fit(x_train, y_train, verbose=1, epochs=50)
-        # print(x_train)
-        # print(y_train)
-        ml.fit(x_train, y_train)
+    #build model
+    # model = net.model(numInputs=8, numOutputs=2)
+    # fit model
+    # print("==================TRAINING=====================")
+    # model.fit(x_train, y_train, verbose=1, epochs=50)
+    # print(x_train)
+    # print(y_train)
+    ml.fit(x_train, y_train)
 
-        # evaluate model on test set
-        # print("===================TESTING=====================")
-        # _, acc = ml.evaluate(x_test, y_test, verbose=1)
-        acc = ml.score(x_test, y_test)
-        # store result
-        #print(model.metrics_names)
-        # print(acc)
-        if (acc > max):
-            max = acc
-            # ml.save("classifier")
-            with open("model.pkl", "wb") as f:
-                pickle.dump(ml, f)
-        results.append(acc)
+    # evaluate model on test set
+    # print("===================TESTING=====================")
+    # _, acc = ml.evaluate(x_test, y_test, verbose=1)
+    acc = ml.score(x_test, y_test)
+    # store result
+    #print(model.metrics_names)
+    # print(acc)
+    # if (acc > max):
+    #     max = acc
+    # ml.save("classifier")
+    with open("model.pkl", "wb") as f:
+        pickle.dump(ml, f)
+    results.append(acc)
     # model.save("classifier")
     return results
 
 
 results = train()
-print(max)
 # print(results)
-print('MAE: %.3f (%.3f)' % (np.mean(results), np.std(results)))
+print('Score: %.3f (%.3f)' % (np.mean(results), np.std(results)))
