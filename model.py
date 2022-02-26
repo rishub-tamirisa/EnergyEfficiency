fa7b4baa91c245e949bd332bd7abb9bdc486f64a
Rishub Tamirisa <rishubt2@illinois.edu>
2022-02-26 02:44:47 -0600

    updated model

--- model.py
+++ model.py
@@ -13,11 +13,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 net = network.Network
 
-# model = network.Network.model(numInputs=8, numOutputs=2)
-# model.build()
-# print(model.summary())
-knn =  DecisionTreeRegressor()#KNeighborsRegressor(n_neighbors=1)
-ml = MultiOutputRegressor(knn, n_jobs=-1)
+input =  DecisionTreeRegressor()
+ml = MultiOutputRegressor(input, n_jobs=-1)
 
 def train_nn():
     x_train, y_train, x_test, y_test = data.Data.create_data()
@@ -42,40 +39,22 @@ def train():
     for i in range(10):
         print("Fold: %i" %(i))
         # prepare data
-        # x_train, y_train, x_test, y_test = data.Data.create_data()
         x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.50,random_state=2)
-
-        #build model
-        # model = net.model(numInputs=8, numOutputs=2)
-        # fit model
-        # print("==================TRAINING=====================")
-        # model.fit(x_train, y_train, verbose=1, epochs=50)
-        # print(x_train)
-        # print(y_train)
         ml.fit(x_train, y_train)
-
         # evaluate model on test set
-        # print("===================TESTING=====================")
-        # _, acc = ml.evaluate(x_test, y_test, verbose=1)
         mae = mean_squared_log_error(y_test, ml.predict(x_test))
         mean_abs_err.append(mae)
         acc = ml.score(x_test, y_test)
-        # store result
-        #print(model.metrics_names)
-        # print(acc)
         if (acc > max):
             max = acc
-            # ml.save("classifier")
             with open("model.pkl", "wb") as f:
                 pickle.dump(ml, f)
         results.append(acc)
-    # model.save("classifier")
     return results, mean_abs_err
 
-
 results, mae = train()
-# print(results)
 print('Score: %.3f (%.3f)' % (np.mean(results), np.std(results)))
 print('EVS: %.3f (%.3f)' % (np.mean(mae), np.std(mae)))
 print("==============================")
 train_nn()
+
