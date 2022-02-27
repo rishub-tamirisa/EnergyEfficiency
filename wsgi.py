88c65b4503e21f38a3e01db065fd9faf2a96293b
Rishub Tamirisa <rishubt2@illinois.edu>
2022-02-27 09:27:27 -0600

    updated wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,5 @@
+import os
 from app import app
-if __name__ == "__app__":
-    app.run()
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
