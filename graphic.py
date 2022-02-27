ce3362b42e69f29b620c88c50a19777fe35eaece
Hritik Arasu <hritikarasu@gmail.com>
2022-02-27 07:02:30 -0600

    Update graphic.py

--- graphic.py
+++ graphic.py
@@ -1,6 +1,5 @@
 from vpython import *
+import app
 
-box(color = vector(1, 0.6, 0), 
-    opacity = 0.5, 
-    shininess = 1, 
-    emissive = False)
+
+box(color = vector(1, 0.6, 0), opacity = 1, shininess = 1, emissive = False)
